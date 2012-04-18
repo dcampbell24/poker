b241d140f18567c43f312dd0adb305b0da88e18f
David Campbell <dcampbell24@gmail.com>
2012-04-18 16:57:36 -0400

    Fixed the combinations test.
    The test was broken since updating the equity code to use ints rather than
    uints.

--- comb/combinations_test.go
+++ comb/combinations_test.go
@@ -6,16 +6,16 @@ import (
 )
 
 func TestGenerator(test *testing.T) {
-	nums := []uint32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
+	nums := []int32{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 	expTotal := Count(big.NewInt(int64(len(nums))), big.NewInt(5))
 	actTotal := new(big.Int)
 
 	c := Generator(nums, 5)
 	loop := true
-	vals := make([]uint32, 5)
+	vals := make([]int32, 5)
 	for loop {
 		loop = c(vals)
-		actTotal.Add(actTotal, ONE)
+		actTotal.Add(actTotal, big.NewInt(1))
 	}
 	if expTotal.Cmp(actTotal) != 0 {
 		test.Fatalf("Expected %v combinations, but saw %v\n", expTotal, actTotal)
